0d8351c8be4b29abba61e7c0cdf003645fbf5ceb
kshitiz dhooria <kshitizdhuria@gmail.com>
2024-05-15 16:41:22 +0530

    das

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -128,7 +128,7 @@ const CreateListing = () => {
       });
 
       /* Send a POST request to server */
-      const response = await fetch("http://localhost:3001/properties/create", {
+      const response = await fetch("https://homerental-8btp.onrender.com/properties/create", {
         method: "POST",
         body: listingForm,
       });
